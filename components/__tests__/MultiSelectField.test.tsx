12b84c91030130a564a4181f320db8608000203b
Aaron Russell <alrussell90@icloud.com>
2024-09-12 23:12:44 -0700

    chore: removed duplicate test file and updated package with jest

--- components/__tests__/MultiSelectField.test.tsx
+++ components/__tests__/MultiSelectField.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import MultiSelectField from './MultiSelectField';
+import MultiSelectField from '../MultiSelectField';
 
 const mockOptions = [
   { value: 'option1', label: 'Option 1' },
