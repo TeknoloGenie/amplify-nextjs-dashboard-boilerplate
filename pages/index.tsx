9d16dedaa3a6b6046b7d4a9ef785ed26fa9e87c8
Aaron Russell <alrussell90@icloud.com>
2024-10-07 23:48:15 -0700

    chore: removed unused code on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,7 @@
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
 import "@aws-amplify/ui-react/styles.css"
 
-const client = generateClient<Schema>();
 
 export default function App() {
-    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-    function listTodos() {
-        client.models.Todo.observeQuery().subscribe({
-            next: (data) => setTodos([...data.items]),
-        });
-    }
-
-    useEffect(() => {
-        listTodos();
-    }, []);
 
     return (
         <>
