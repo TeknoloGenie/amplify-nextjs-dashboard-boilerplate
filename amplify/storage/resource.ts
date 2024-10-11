8ed97cf5b39c7f83766e66418e597b26cb75660b
Aaron Russell <alrussell90@icloud.com>
2024-10-10 23:37:22 -0700

    fix: build error

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from "@aws-amplify/backend-storage";
 export const componentConfigStorage = defineStorage({
   name: "ComponentConfigStorage",
   access: (allow) => ({
-    "*/*": [
+    "/*": [
       allow.authenticated.to(["read", "write"]),
     ]
   })
