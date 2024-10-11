65bbb828a6f302664ceac94c0c8848ed0facde95
Aaron Russell <alrussell90@icloud.com>
2024-10-11 02:00:57 -0700

    fix(eslint): empty component files not aloud for eslint

---
+++ pages/dashboards/support/index.tsx
@@ -0,0 +1,11 @@
+import BaseDashboard from "../base";
+
+const SupportDashboard: React.FC<typeof BaseDashboard> = () => {
+    return (
+        <div>
+            <h1>Support Dashboard</h1>
+        </div>
+    )
+}
+
+export default SupportDashboard;
